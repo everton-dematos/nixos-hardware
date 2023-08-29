058074adacd79e0614a87272c3212b982aa0d76d
everton-dematos <everton@ssrc.tii.ae>
2023-08-29 15:56:32 +0400

    Testing x88 sel4payload
    Signed-off-by: everton-dematos <everton@ssrc.tii.ae>

--- tii/tc-saluki/uboot/default.nix
+++ tii/tc-saluki/uboot/default.nix
@@ -31,7 +31,7 @@ buildUBoot rec {
   enableParallelBuilding = true;
   extraMeta.platforms = ["riscv64-linux"];
   postBuild = ''
-        cp ${sel4_local}/teeos_root-image-riscv-polarfire .
+        cp ${sel4_local}/sel4payload .
         ${payload-generator}/hss-payload-generator -c ${payload_config} payload.bin
         '';
   filesToInstall = [ "payload.bin" ];
