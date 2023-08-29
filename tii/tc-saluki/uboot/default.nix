9597383d224572df3b7c071662b33c6df981c2e2
everton-dematos <everton@ssrc.tii.ae>
2023-08-29 14:01:21 +0400

    Testing teeos_root-image-riscv-polarfire
    Signed-off-by: everton-dematos <everton@ssrc.tii.ae>

--- tii/tc-saluki/uboot/default.nix
+++ tii/tc-saluki/uboot/default.nix
@@ -5,7 +5,7 @@
 with pkgs; let
   payload-generator = pkgs.callPackage ./hss-payload-generator.nix {};
   #sel4 = pkgs.callPackage ./sel4.nix {};
-  sel4_local = /home/ssrclab1/Documents/seL4/RISC-V/seL4_TEE_platsecdev_OS/build/gen_hss_payload;
+  sel4_local = /home/ssrclab1/Documents/seL4/RISC-V/seL4_TEE_platsecdev_OS/build/images;
   payload_config = ./uboot.yaml;
 in
 buildUBoot rec {
@@ -31,9 +31,9 @@ buildUBoot rec {
   enableParallelBuilding = true;
   extraMeta.platforms = ["riscv64-linux"];
   postBuild = ''
-        cp ${sel4_local}/elfloader .
-        ls
+        cp ${sel4_local}/teeos_root-image-riscv-polarfire .
         ${payload-generator}/hss-payload-generator -c ${payload_config} payload.bin
         '';
   filesToInstall = [ "payload.bin" ];
+  filesToInstall = [ "teeos_root-image-riscv-polarfire" ];
 }
