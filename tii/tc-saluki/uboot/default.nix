0003c50af89e1dc5e0b2a447091bb52487cb6663
everton-dematos <everton@ssrc.tii.ae>
2023-08-29 12:40:13 +0400

    Testing sel4OS payload
    Signed-off-by: everton-dematos <everton@ssrc.tii.ae>

--- tii/tc-saluki/uboot/default.nix
+++ tii/tc-saluki/uboot/default.nix
@@ -5,7 +5,7 @@
 with pkgs; let
   payload-generator = pkgs.callPackage ./hss-payload-generator.nix {};
   #sel4 = pkgs.callPackage ./sel4.nix {};
-  sel4_local = /home/ssrclab1/Documents/seL4/RISC-V/sel4test-supported_platforms/cbuild/images;
+  sel4_local = /home/ssrclab1/Documents/seL4/RISC-V/seL4_TEE_platsecdev_OS/build/gen_hss_payload;
   payload_config = ./uboot.yaml;
 in
 buildUBoot rec {
@@ -31,7 +31,7 @@ buildUBoot rec {
   enableParallelBuilding = true;
   extraMeta.platforms = ["riscv64-linux"];
   postBuild = ''
-        cp ${sel4_local}/sel4test-driver-image-riscv-polarfire .
+        cp ${sel4_local}/sel4payload .
         ${payload-generator}/hss-payload-generator -c ${payload_config} payload.bin
         '';
   filesToInstall = [ "payload.bin" ];
