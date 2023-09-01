e434101016bb86d454bc863097e103365a4366e8
everton-dematos <everton@ssrc.tii.ae>
2023-09-01 15:59:17 +0400

    Testing skip-opensbi: true sel4payload_AMP
    Signed-off-by: everton-dematos <everton@ssrc.tii.ae>

--- tii/tc-saluki/uboot/default.nix
+++ tii/tc-saluki/uboot/default.nix
@@ -35,7 +35,7 @@ buildUBoot rec {
   enableParallelBuilding = true;
   extraMeta.platforms = ["riscv64-linux"];
   postBuild = ''
-        cp ${sel4_local}/seL4.bin .
+        cp ${sel4_local}/sel4payload_AMP .
         ${payload-generator}/hss-payload-generator -c ${payload_config} payload.bin
         '';
   filesToInstall = [ "payload.bin" ];
