2a34b8882cedeee662fa8ec9d58439e1a1b990f9
everton-dematos <everton@ssrc.tii.ae>
2023-09-01 13:49:15 +0400

    Testing seL4-uImage
    Signed-off-by: everton-dematos <everton@ssrc.tii.ae>

--- tii/tc-saluki/uboot/default.nix
+++ tii/tc-saluki/uboot/default.nix
@@ -35,7 +35,7 @@ buildUBoot rec {
   enableParallelBuilding = true;
   extraMeta.platforms = ["riscv64-linux"];
   postBuild = ''
-        cp ${sel4_local}/sel4test-tutorial .
+        cp ${sel4_local}/seL4-uImage .
         ${payload-generator}/hss-payload-generator -c ${payload_config} payload.bin
         '';
   filesToInstall = [ "payload.bin" ];
