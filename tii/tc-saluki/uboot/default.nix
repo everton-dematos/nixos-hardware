6b5702c7c23d358e96ddaf1c8198b687f9c8e947
everton-dematos <everton@ssrc.tii.ae>
2023-10-29 19:21:09 +0400

    Testing sel4test_oct20
    Signed-off-by: everton-dematos <everton@ssrc.tii.ae>

--- tii/tc-saluki/uboot/default.nix
+++ tii/tc-saluki/uboot/default.nix
@@ -36,7 +36,7 @@ buildUBoot rec {
   enableParallelBuilding = true;
   extraMeta.platforms = ["riscv64-linux"];
   postBuild = ''
-        cp ${sel4_local}/hello.elf .
+        cp ${sel4_local}/sel4test_oct20 .
         ${payload-generator}/hss-payload-generator -c ${payload_config} payload.bin
         '';
   filesToInstall = [ "payload.bin" ];
