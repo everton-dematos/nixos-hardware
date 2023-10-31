931eaa2e464de9d041cfecdf79ae03cf8a307197
everton-dematos <everton@ssrc.tii.ae>
2023-10-31 17:03:49 +0400

    Testing seL4_AMP.bin
    Signed-off-by: everton-dematos <everton@ssrc.tii.ae>

--- tii/tc-saluki/uboot/default.nix
+++ tii/tc-saluki/uboot/default.nix
@@ -36,7 +36,7 @@ buildUBoot rec {
   enableParallelBuilding = true;
   extraMeta.platforms = ["riscv64-linux"];
   postBuild = ''
-        cp ${sel4_local}/seL4.bin .
+        cp ${sel4_local}/seL4_AMP.bin .
         ${payload-generator}/hss-payload-generator -c ${payload_config} payload.bin
         '';
   filesToInstall = [ "payload.bin" ];
