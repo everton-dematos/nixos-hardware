c12984791ce5980fb1e1b9c3006e7ed167af2fc0
everton-dematos <everton@ssrc.tii.ae>
2023-10-31 14:23:05 +0400

    Testing sel4OS_mk_permission
    Signed-off-by: everton-dematos <everton@ssrc.tii.ae>

--- tii/tc-saluki/uboot/default.nix
+++ tii/tc-saluki/uboot/default.nix
@@ -36,7 +36,7 @@ buildUBoot rec {
   enableParallelBuilding = true;
   extraMeta.platforms = ["riscv64-linux"];
   postBuild = ''
-        cp ${sel4_local}/sel4OS_mk .
+        cp ${sel4_local}/sel4OS_mk_permission .
         ${payload-generator}/hss-payload-generator -c ${payload_config} payload.bin
         '';
   filesToInstall = [ "payload.bin" ];
