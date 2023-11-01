1f11a22c817f7a93c33876c2b73522c1a8924746
everton-dematos <everton@ssrc.tii.ae>
2023-11-01 16:52:08 +0400

    Testing seL4_Oct31.bin skip-opensbi
    Signed-off-by: everton-dematos <everton@ssrc.tii.ae>

--- tii/tc-saluki/uboot/default.nix
+++ tii/tc-saluki/uboot/default.nix
@@ -36,7 +36,7 @@ buildUBoot rec {
   enableParallelBuilding = true;
   extraMeta.platforms = ["riscv64-linux"];
   postBuild = ''
-        cp ${sel4_local}/seL4_AMP.bin .
+        cp ${sel4_local}/seL4_Oct31.bin .
         ${payload-generator}/hss-payload-generator -c ${payload_config} payload.bin
         '';
   filesToInstall = [ "payload.bin" ];
