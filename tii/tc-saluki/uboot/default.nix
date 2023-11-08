4e37c08020339be25f34883a6dbe74dfe16a8b98
everton-dematos <everton@ssrc.tii.ae>
2023-11-08 14:48:57 +0400

    Testing kernel.elf
    Signed-off-by: everton-dematos <everton@ssrc.tii.ae>

--- tii/tc-saluki/uboot/default.nix
+++ tii/tc-saluki/uboot/default.nix
@@ -5,7 +5,7 @@
 with pkgs; let
   payload-generator = pkgs.callPackage ./hss-payload-generator.nix {};
   #sel4 = pkgs.callPackage ./sel4.nix {};
-  sel4_local = /home/ssrclab1/Documents/seL4/RISC-V/img_bckp/images;
+  sel4_local = /home/ssrclab1/Documents/seL4/RISC-V/img_bckp;
   payload_config = ./uboot.yaml;
 in
 buildUBoot rec {
@@ -36,9 +36,7 @@ buildUBoot rec {
   enableParallelBuilding = true;
   extraMeta.platforms = ["riscv64-linux"];
   postBuild = ''
-        cp ${sel4_local}/seL4.bin .
-        cp ${sel4_local}/mpfs-rpmsg-remote.elf .
-        cp ${sel4_local}/rootfs.tar .
+        cp ${sel4_local}/kernel.elf .
         ${payload-generator}/hss-payload-generator -c ${payload_config} payload.bin
         '';
   filesToInstall = [ "payload.bin" ];
